57a6fb6e8c853a799d547b4448d59983bc3989e1
fanchy <zxfown@gmail.com>
2018-01-13 20:39:17 +0800

    more for test task

--- main.py
+++ main.py
@@ -62,3 +62,20 @@ multigetRet = h2ext.callFunc("Cache.multiget", ["m.n", "b"])
 print("multigetRet", multigetRet)
 cacheRet = h2ext.callFunc("Cache.get", "")
 print("cacheRet", cacheRet, h2ext.callFunc("Cache.size", "m"), h2ext.callFunc("Cache.size", "m.n"))
+
+def test(arg = None, arg2 = None):
+    print('test func', arg, arg2)
+    return arg
+
+def testTask(entity):
+    h2ext.callFunc("Task.addTaskCfg", {"cfgid":1, "taskLine":2, "nextTask":2, "finishConditionType":"killMonster", "finishConditionObject":"cat", "finishConditionValue":1})
+    h2ext.callFunc("Task.addTaskCfg", {"cfgid":2, "taskLine":2, "nextTask":0, "finishConditionType":"killMonster", "finishConditionObject":"dog", "finishConditionValue":1})
+    taskCfg = h2ext.callFunc("Task.getTaskCfg", 1)
+    print("taskCfg", taskCfg)
+    h2ext.callFunc("Task.genTask", entity, 1)
+    h2ext.callFunc("Task.acceptTask", entity, 1)
+    h2ext.callFunc("Task.triggerEvent", entity, 'killMonster', 'cat', 1)
+    h2ext.callFunc("Task.endTask", entity, 1)
+    allTask = h2ext.callFunc("Task.getTask", entity, 2)
+    print("testTask allTask", allTask)
+    return
