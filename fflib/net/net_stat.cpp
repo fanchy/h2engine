3f73e1a5b908efd0432cd0eb92a1af1885273d20
fanchy <zxfown@gmail.com>
2018-04-08 10:16:53 +0800

    网友周华找到的一处参数错误

--- fflib/net/net_stat.cpp
+++ fflib/net/net_stat.cpp
@@ -41,7 +41,7 @@ int NetStat::start(ArgHelper& arg_helper_)
     
     m_timerService = new TimerService();
     
-    m_timerService->timerCallback(m_heartbeat.timeout(), Task(&timer_check, (void*)this));
+    m_timerService->timerCallback(m_heartbeat.timeout()*1000, Task(&timer_check, (void*)this));
     return 0;
 }
 
@@ -51,7 +51,7 @@ int NetStat::start(int max_packet_size, int heartbeat_timeout)
     m_heartbeat.set_option(heartbeat_timeout, timer_close);
     m_timerService = new TimerService();
     
-    m_timerService->timerCallback(m_heartbeat.timeout(), Task(&timer_check, (void*)this));
+    m_timerService->timerCallback(m_heartbeat.timeout()*1000, Task(&timer_check, (void*)this));
     return 0;
 }
 void NetStat::handleTimerCheck()
